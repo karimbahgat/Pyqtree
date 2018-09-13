77eb4a1690488f7c4385a4a437e294c3c1169c74
Karim Bahgat <karim.bahgat.norway@gmail.com>
2018-09-12 20:18:48 -0400

    Better testing

--- pyqtree_test.py
+++ pyqtree_test.py
@@ -17,23 +17,25 @@ def test_should_add_single_node_and_find_its_intersection():
     index = Index(INDEX_BBOX)
     index.insert(ITEM1, BBOX1)
     assert len(index) == 1
-    assert index.intersect(BBOX1) == {ITEM1}
+    assert index.intersect(BBOX1) == [ITEM1]
 
 def test_should_add_multiple_nodes_and_find_them():
     index = Index(INDEX_BBOX)
     index.insert(ITEM1, BBOX1)
     index.insert(ITEM2, BBOX2)
     assert len(index) == 2
-    assert index.intersect(BBOX1) == {ITEM1}
-    assert index.intersect(BBOX2) == {ITEM2}
-    assert index.intersect(INDEX_BBOX) == {ITEM1, ITEM2}
+    assert index.intersect(BBOX1) == [ITEM1]
+    assert index.intersect(BBOX2) == [ITEM2]
+    res = index.intersect(INDEX_BBOX)
+    assert ITEM1 in res
+    assert ITEM2 in res
 
 def test_should_empty_index_after_removing_added_node():
     index = Index(INDEX_BBOX)
     index.insert(ITEM1, BBOX1)
     index.remove(ITEM1, BBOX1)
     assert len(index) == 0
-    assert index.intersect(BBOX1) == set()
+    assert index.intersect(BBOX1) == []
 
 def test_should_empty_index_after_removing_multiple_added_nodes_in_separate_quad_nodes():
     index = Index(INDEX_BBOX, max_items=1)
@@ -42,7 +44,7 @@ def test_should_empty_index_after_removing_multiple_added_nodes_in_separate_quad
     index.remove(ITEM1, BBOX1)
     index.remove(ITEM2, BBOX2)
     assert len(index) == 0
-    assert index.intersect(INDEX_BBOX) == set()
+    assert index.intersect(INDEX_BBOX) == []
 
 def test_should_empty_index_after_removing_multiple_added_nodes_in_all_quad_nodes():
     index = Index(INDEX_BBOX, max_items=1)
@@ -51,7 +53,7 @@ def test_should_empty_index_after_removing_multiple_added_nodes_in_all_quad_node
     index.remove(ITEM1, BBOX1)
     index.remove(ITEM2, INDEX_BBOX)
     assert len(index) == 0
-    assert index.intersect(INDEX_BBOX) == set()
+    assert index.intersect(INDEX_BBOX) == []
 
 def test_should_empty_index_after_removing_multiple_added_nodes_in_multiple_horizontal_quad_nodes():
     index = Index(INDEX_BBOX, max_items=1)
@@ -61,7 +63,7 @@ def test_should_empty_index_after_removing_multiple_added_nodes_in_multiple_hori
     index.remove(ITEM1, BBOX1)
     index.remove(ITEM2, bbox2)
     assert len(index) == 0
-    assert index.intersect(INDEX_BBOX) == set()
+    assert index.intersect(INDEX_BBOX) == []
 
 def test_should_empty_index_after_removing_multiple_added_nodes_in_multiple_vertical_quad_nodes():
     index = Index(INDEX_BBOX, max_items=1)
@@ -71,4 +73,21 @@ def test_should_empty_index_after_removing_multiple_added_nodes_in_multiple_vert
     index.remove(ITEM1, BBOX1)
     index.remove(ITEM2, bbox2)
     assert len(index) == 0
-    assert index.intersect(INDEX_BBOX) == set()
+    assert index.intersect(INDEX_BBOX) == []
+
+if __name__ == '__main__':
+    import traceback
+    tests = [(name,obj) for name,obj in list(globals().items()) if hasattr(obj, '__call__') and obj.__name__.startswith('test_')]
+    failed = 0
+    for name,obj in tests:
+        try:
+            obj()
+        except Exception as err:
+            failed += 1
+            print('test fail: "{}"'.format(name))
+            traceback.print_exc()
+
+    if failed:
+        print('{} out of {} tests failed'.format(failed, len(tests)))
+    else:
+        print('All tests completed successfully')
